8a0a455d667e7a297c1f073d2deac7095e1d4dd2
Kongpob <kongpop.rak@hotmail.co.th>
2020-10-20 22:34:36 +0700

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  get  'auth/:provider/callback' => 'sessions#create'
+  post 'logout' => 'sessions#destroy'
+  get  'auth/failure' => 'sessions#failure'
+  get  'auth/twitter', :as => 'login'
+  
+  get '/movies/search_tmdb' => 'movies#search_tmdb'
+  post '/movies/createfromtmdb' => 'movies#create_from_tmdb', :as => 'createfromtmdb'
+  
+  resources :movies do
+    resources :reviews
+  end
+  root :to => redirect('/movies')
 end
